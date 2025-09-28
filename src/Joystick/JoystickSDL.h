38edae4e610bbdc0a7cd673f4e1c6aa44b2dda7b
Thinhhoagn0211 <chienvuong199@gmail.com>
2025-09-28 23:44:57 +0700

    test

---
+++ src/Joystick/JoystickSDL.h
@@ -0,0 +1,57 @@
+/****************************************************************************
+ *
+ * (c) 2009-2024 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
+ *
+ * QGroundControl is licensed according to the terms in the file
+ * COPYING.md in the root of the source code directory.
+ *
+ ****************************************************************************/
+
+#pragma once
+
+#include <QtCore/QLoggingCategory>
+#include "MultiVehicleManager.h"
+#include "Joystick.h"
+
+struct SDL_Joystick;
+typedef struct SDL_Joystick SDL_Joystick;
+
+struct SDL_Gamepad;
+typedef struct SDL_Gamepad SDL_Gamepad;
+
+Q_DECLARE_LOGGING_CATEGORY(JoystickSDLLog)
+
+class JoystickSDL : public Joystick
+{
+public:
+    explicit JoystickSDL(const QString &name, QList<int> gamepadAxes, QList<int> nonGamepadAxes, int buttonCount, int hatCount, int instanceId, bool isGamepad, QObject *parent = nullptr);
+    ~JoystickSDL() override;
+
+    int instanceId() const { return _instanceId; }
+    void setInstanceId(int instanceId) { _instanceId = instanceId; }
+
+    // bool requiresCalibration() const final { return !_isGamepad; }
+
+    static bool init();
+    static QMap<QString, Joystick*> discover(MultiVehicleManager* _multiVehicleManager); 
+
+private:
+    bool _open() final;
+    void _close() final;
+    bool _update() final;
+
+    bool _getButton(int idx) const final;
+    int _getAxis(int idx) const final;
+    bool _getHat(int hat, int idx) const final;
+
+    static void _loadGamepadMappings();
+
+    QList<int> _gamepadAxes;
+    QList<int> _nonGamepadAxes;
+    bool _isGamepad = false;
+    int _instanceId = -1;
+    bool    _isGameController;
+    int     _index;
+    SDL_Joystick*       sdlJoystick;
+    SDL_GameController* sdlController;
+};
---
+++ src/MAVLink/LibEvents/libevents_includes.h
@@ -0,0 +1,29 @@
+/****************************************************************************
+ *
+ * (c) 2009-2024 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
+ *
+ * QGroundControl is licensed according to the terms in the file
+ * COPYING.md in the root of the source code directory.
+ *
+ ****************************************************************************/
+
+#pragma once
+
+#include <cstdlib>
+#include <cstdarg>
+
+#include <QtCore/QLoggingCategory>
+
+Q_DECLARE_LOGGING_CATEGORY(EventsLog)
+
+void qgc_events_parser_debug_printf(const char *fmt, ...);
+
+//#define LIBEVENTS_PARSER_DEBUG_PRINTF qgc_events_parser_debug_printf
+#define LIBEVENTS_DEBUG_PRINTF qgc_events_parser_debug_printf
+
+#include <MAVLinkLib.h>
+
+#include "libevents/libs/cpp/protocol/receive.h"
+#include "libevents/libs/cpp/parse/health_and_arming_checks.h"
+#include "libevents/libs/cpp/parse/parser.h"
+#include "libevents/libs/cpp/generated/events_generated.h"
